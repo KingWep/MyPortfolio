0cbaafb68c9d6f6fb319540509c7fffda29c558e
King Wep <thaneverlose168@gmail.com>
2026-06-01 15:54:15 +0700

    Create Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App(){
+  return(
+  <>
+    <h1 className="text-3xl bg-blue-500 text-red-800 font-bold underline">
+      Hello world!
+    </h1>
+    <div className="text-2xl text-yellow-800 font-bold underline">
+      Halajks,jid
+    </div>
+  </>
+  )
+}
+import React from 'react'
